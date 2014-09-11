85d859bad96b320f91998819a87dd5e0e62773a0
jeffery <dungeonsnd@gmail.com>
2014-09-12 00:15:32 +0800

    add dbpool

--- develop/examples/pyserver/src/pys_common.py
+++ develop/examples/pyserver/src/pys_common.py
@@ -17,3 +17,38 @@ def recvall(sock,totalLen):
         left =totalLen-len(data)
     return data
 
+
+import pymysql
+from gevent import monkey
+monkey.patch_socket() 
+from gevent.queue import Queue
+
+class SimpleDBPool():
+    HOST='localhost'
+    PASSWD='pwd'
+    USER='root'
+    DB='test'
+    def __init__(self):
+        self._dbFree =Queue()
+        for conn in [pymysql.connect(host=HOST,passwd=PASSWD,user=USER,db=DB) for i in xrange(0,10)] :
+            self._dbFree.put(conn)
+        print dbFreeQueue
+        
+    def Get():
+        conn =self.dbFreeQueue.get()
+        return conn
+    def Release(conn):
+        dbFreeQueue.put(conn)
+        gevent.sleep(0)
+        
+// TODO:
+def dbQuery1(sql,db): 
+    print '---- dbQuery1 start ----'
+    cursor = db.cursor() 
+    data = cursor.execute(sql) 
+    cursor.close() 
+    db.commit()
+    for e in cursor:
+        print e 
+    print '---- dbQuery1 over ----'
+        
